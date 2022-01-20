0fbc025dca0733b08742a06cbb931293b1f6ecad
forana <alex@saturncloud.io>
2022-01-20 08:58:02 -0600

    add schema + example for resource sets, tweak CI for relative import

--- .ci/validate-examples.py
+++ .ci/validate-examples.py
@@ -1,19 +1,31 @@
 import json
 from pathlib import Path
+from typing import Any, Dict
 
-from jsonschema import validate
+from jsonschema import RefResolver, Validator
+from jsonschema.validators import validator_for
 
 
+schemas: Dict[str, Any] = {}
+examples: Dict[str, Any] = {}
+
+# load files
 root = Path(__file__).parent.parent
-
 for d in root.iterdir():
     if d.is_dir() and not str(d).startswith("."):
-        print(f"validating '{d}'...")
         with open(root / d / "schema.json") as f:
-            schema = json.load(f)
+            schemas[str(d)] = json.load(f)
         with open(root / d / "example.json") as f:
-            instance = json.load(f)
+            examples[str(d)] = json.load(f)
 
-        validate(instance=instance, schema=schema)
-        print("success!")
+# validate files
+for r, s in schemas.items():
+    print(f"validating '{r}'...")
+    resolver = RefResolver(f"{r}/schema.json", s)
+    # 'resource-sets' needs a relative import, which jsonschema doesn't natively support
+    if r == "resource-sets":
+        resolver.store["/resources/schema.json"] = schemas["resources"]
+    validator: Validator = validator_for(s)(s, resolver)
+    validator.validate(examples[r])
+    print("success!")
 
