6344d9cc1c3261d8e9c647c4649817ae89bf77c4
Julia Signell <jsignell@gmail.com>
2022-01-28 17:17:07 -0500

    Edit image schema (#20)

--- .ci/validate-examples.py
+++ .ci/validate-examples.py
@@ -6,6 +6,22 @@ from jsonschema import validate
 
 root = Path(__file__).parent.parent
 
+
+def mandatoryAdditionalProperties(schema, name):
+    if "properties" not in schema:
+        return
+    if schema.get("additionalProperties", True):
+        raise ValueError(
+            "Please set additionalProperties=false everwhere! \n"
+            f"* Missing in schema: {name}"
+        )
+    for field, value in schema["properties"].items():     
+        if value.get("type") == "array":
+            for item in value["items"]:
+                mandatoryAdditionalProperties(item, f"{name}.{field}")
+        else:
+            mandatoryAdditionalProperties(value, f"{name}.{field}")
+
 for d in root.iterdir():
     if d.is_dir() and not str(d).startswith("."):
         print(f"validating '{d}'...")
@@ -13,6 +29,8 @@ for d in root.iterdir():
             schema = json.load(f)
         with open(root / d / "example.json") as f:
             instance = json.load(f)
+
+        mandatoryAdditionalProperties(schema, d)
 
         validate(instance=instance, schema=schema)
         print("success!")
